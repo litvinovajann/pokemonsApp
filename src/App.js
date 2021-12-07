aa6361a2dad210b22f09461c3e4cb8c4305e68de
litvinovajann <litvinovajann@gmail.com>
2021-12-07 15:56:56 +0300

    added caughtPokemons component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import { Pokemon } from './components/onePokemon';
 import {pokemonsData} from './data/PokemonsData.js';
+import {CaugthPokemons} from './components/caughtPokemons';
 export function App() {
-    return ( <div className="pokemonsTable">
+    return ( <div className="wrapper">
+        <CaugthPokemons />
+        <div className="pokemonsTable">
         {pokemonsData.map(onePieceOfPokemon => ( 
         <Pokemon 
         key={onePieceOfPokemon.id}
         name={onePieceOfPokemon.name}
         id = {onePieceOfPokemon.id}
         />))}
+        </div>
       </div>
     );
 }
---
+++ src/components/caughtPokemons.js
@@ -0,0 +1,8 @@
+export function CaugthPokemons() {
+    return ( <div
+        className="caughtPokemons"
+        >
+        Поймано покемонов: 0/10
+      </div>
+    );
+}
